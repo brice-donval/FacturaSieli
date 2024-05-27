6799c0e3823bbf5273db59fe8e2a59e601c049b2
Brice Donval <54807091+brice-donval@users.noreply.github.com>
2024-05-27 15:04:07 +0200

    Update notification_service.py

--- Django project/facturasieli/services/notification_service.py
+++ Django project/facturasieli/services/notification_service.py
@@ -1,14 +1,16 @@
+# facturasieli/services/notification_service.py
 from datetime import datetime
 
 from django.shortcuts import get_object_or_404
 
-from ..models import Notification, Company
+from facturasieli.models import Company, Notification
+
 
 @staticmethod
 def send_notification(notification_type:str, service_title:str,company_sender_id:int, company_receiver_id:int):
     company_sender = get_object_or_404(Company, pk=company_sender_id)
     company_receiver = get_object_or_404(Company, pk=company_receiver_id)
-   
+
     notification = Notification()
     notification.send_at = datetime.now()
     notification.type=notification_type

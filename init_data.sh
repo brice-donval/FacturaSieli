63b588a5355eba340e97776e9de9bb21f713c0e0
Brice Donval <54807091+brice-donval@users.noreply.github.com>
2024-05-27 12:00:21 +0200

    Update init_data.sh

--- init_data.sh
+++ init_data.sh
@@ -1,10 +1,8 @@
-.venv/bin/python  Django\ projet/manage.py loaddata ../initial_data/Addresses.json
-.venv/bin/python 'Django project'/manage.py loaddata initial_data/Companies.json
-.venv/bin/python 'Django project'/manage.py loaddata initial_data/Roles.json
-.venv/bin/python 'Django project'/manage.py loaddata initial_data/Users.json
-.venv/bin/python 'Django project'/manage.py loaddata initial_data/Services.json
-.venv/bin/python 'Django project'/manage.py loaddata initial_data/Invoices.json
-
-
-.venv/bin/python 'Django project'/manage.py loaddata initial_data/Notifications.json
-.venv/bin/python 'Django project'/manage.py loaddata initial_data/Verifications.json
+.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Addresses.json
+.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Companies.json
+.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Invoices.json
+.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Notifications.json
+.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Roles.json
+.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Services.json
+.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Users.json
+.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Verifications.json

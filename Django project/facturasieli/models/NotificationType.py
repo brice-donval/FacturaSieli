004cd51d6d4dd9075540133888a873e0059775df
Brice Donval <54807091+brice-donval@users.noreply.github.com>
2024-05-27 11:51:02 +0200

    Update NotificationType.py

--- Django project/facturasieli/models/NotificationType.py
+++ Django project/facturasieli/models/NotificationType.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class NotificationType(models.IntegerChoices):
     DEMANDE_FACTURE = 1, _('Invoice Request')
     FACTURE_SOUMISE = 2, _('Invoice Submitted')

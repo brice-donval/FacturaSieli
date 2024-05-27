e3c75064825c24b2dd0d9ef0206df98fc2e41120
Maurice Ambroise <maurice.ambroise79@gmail.com>
2024-05-27 11:46:12 +0200

    Le formulaire doit inclure les champs nécessaires : titre du service, description, sous-traitant associé..

--- Django project/facturasieli/forms/ServiceForm.py
+++ Django project/facturasieli/forms/ServiceForm.py
@@ -1,2 +1,16 @@
-from django.forms import ModelForm
+from django import forms
+from ..models.Service import Service
+from django.utils.translation import gettext_lazy as _
 
+# Author Morice
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Service
+        fields = ["title", "description", "intervention_start_date", "intervention_end_date", "company_client"]  # Corrected typo here
+        labels = {
+            'title': _('Service Title:'),
+            'description': _('Description:'),
+            'intervention_start_date': _('Start of the service:'),
+            'intervention_end_date': _('End of the service:'),
+            'company_client': _('Company:'),
+        }
--- Django project/facturasieli/views/service/service_form_view.py
+++ Django project/facturasieli/views/service/service_form_view.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
+from ...forms.ServiceForm import ServiceForm
 
 # author : Morice
 
 
 def handle_service(request):
     if request.method == 'POST':
-        pass
-    return render(request, 'facturasieli/service_form.html')
+        form =ServiceForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            intervention_start_date = form.cleaned_data['intervention_start_date']
+            intervention_end_date = form.cleaned_data['intervention_end_date']
+            company_client = form.cleaned_data['company_client']
+    else:
+        form = ServiceForm()
+    return render(request, 'facturasieli/service_form.html', {"form":form})

ba27d2f8a06236c55a971a697720a4da77496be2
zineb <zineb.soudi@gmail.com>
2024-05-27 11:45:50 +0200

    changement de langue dans verificationForm.py

--- Django project/facturasieli/forms/VerificationForm.py
+++ Django project/facturasieli/forms/VerificationForm.py
@@ -2,14 +2,15 @@
 from django import forms
 from facturasieli.models import Verification
 
+
 class VerificationForm(forms.ModelForm):
     class Meta:
         model = Verification
         fields = ['comments']
 
     STATUS_CHOICES = [
-        ('vérifiée', 'Vérifiée'),
-        ('réjetée', 'Réjetée')
+        (1, 'Verified'),
+        (2, 'rejected')
     ]
     
     status = forms.ChoiceField(choices=STATUS_CHOICES, widget=forms.RadioSelect)

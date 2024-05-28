f8b538c618710fada691f1961a817954ab3f3330
brice-donval <b.donval@wanadoo.fr>
2024-05-28 14:36:53 +0200

    Bugfix : the loading order was important!

--- init_data.sh
+++ init_data.sh
@@ -1,8 +1,11 @@
-.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Addresses.json
-.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Companies.json
-.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Invoices.json
-.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Notifications.json
-.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Roles.json
-.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Services.json
-.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Users.json
-.venv/bin/python3 'Django project'/manage.py loaddata initial_data/Verifications.json
+#!/bin/bash
+
+.venv/bin/python 'Django project'/manage.py loaddata initial_data/Addresses.json
+.venv/bin/python 'Django project'/manage.py loaddata initial_data/Companies.json
+.venv/bin/python 'Django project'/manage.py loaddata initial_data/Roles.json
+.venv/bin/python 'Django project'/manage.py loaddata initial_data/Users.json
+.venv/bin/python 'Django project'/manage.py loaddata initial_data/Services.json
+.venv/bin/python 'Django project'/manage.py loaddata initial_data/Invoices.json
+
+.venv/bin/python 'Django project'/manage.py loaddata initial_data/Notifications.json
+.venv/bin/python 'Django project'/manage.py loaddata initial_data/Verifications.json

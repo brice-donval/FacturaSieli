b648b376bd6b2eb680b8988d473b7a7dd107d9d8
Brice Donval <54807091+brice-donval@users.noreply.github.com>
2024-05-29 11:56:47 +0200

    Update InvoiceForm.py

--- Django project/facturasieli/forms/InvoiceForm.py
+++ Django project/facturasieli/forms/InvoiceForm.py
@@ -1,6 +1,14 @@
-# facturasieli/forms/InvoiceForm.py
+# ---------------------------------------------------------------------------
+#                    F a c t u r a S i e l i   ( 2 0 2 4 )
+# ---------------------------------------------------------------------------
+# File   : facturasieli/forms/InvoiceForm.py
+# Author : Margaux
+# ---------------------------------------------------------------------------
+
 from django import forms
+
 from facturasieli.models.Invoice import Invoice
+
 
 class InvoiceForm(forms.ModelForm):
     class Meta:
